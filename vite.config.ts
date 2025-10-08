b2226593bf5d0b0df311692d409e3738100f209e
RnArauj0 <u202113612@upc.edu.pe>
2025-10-07 23:22:52 -0500

    Refactor: update build scripts and improve path alias in package.json and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,17 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-    base: "/landing-page/", // 👈 IMPORTANTE
-
+    base: "/landing-page/",
     server: {
         host: "::",
         port: 8080,
     },
-
     plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-
     resolve: {
         alias: {
-            "@": path.resolve(__dirname, "src"),
+            "@": path.resolve(__dirname, "./src"),
         },
     },
 }));
