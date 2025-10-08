d4fdb6d5a0d38d22a7b0deb7d8a03423569d4000
RnArauj0 <u202113612@upc.edu.pe>
2025-10-07 23:57:49 -0500

    Refactor: correct formatting of resolve section in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
         outDir: "docs",
     },
     plugins: [react()],
-    resolve: {
+    resolve:
         alias: {
             "@": path.resolve(__dirname, "./src"),
         },
